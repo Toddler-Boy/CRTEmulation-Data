97b6580e54ac8fed692e15e94658423a93f9882e
Michael Hartmann <mike@ultrasid.com>
2026-05-02 08:43:53 -0700

    Added bitmaps, overlays, shaders, etc.

---
+++ Shaders/lights.glsl
@@ -0,0 +1,16 @@
+#include "includes/getMipMapColor.glsl"
+
+uniform	float	ovlBloom = 1.0;
+
+//-----------------------------------------------------------------------------
+
+void main ()
+{
+	vec4	bloom = getMipMapColor ( iChannel0, fragCoord, 1.0 );
+	bloom += getMipMapColor ( iChannel0, fragCoord, 2.0 );
+	bloom += getMipMapColor ( iChannel0, fragCoord, 3.0 );
+	bloom *= ovlBloom;
+
+	fragColor = bloom + texture ( iChannel0, fragCoord );
+}
+//-----------------------------------------------------------------------------
